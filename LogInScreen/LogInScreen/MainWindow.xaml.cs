9b1e0a25f205d96fbbffae30dd180fab20777a57
Michael Walsh <calex71@gmail.com>
2018-11-25 14:51:15 +0000

    Adjusted background and font colours. Get a second opinion here maybe before merging.

--- LogInScreen/LogInScreen/MainWindow.xaml.cs
+++ LogInScreen/LogInScreen/MainWindow.xaml.cs
@@ -18,6 +18,8 @@ namespace LogInScreen
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
+    
     public partial class MainWindow : Window
     {
         public MainWindow()
