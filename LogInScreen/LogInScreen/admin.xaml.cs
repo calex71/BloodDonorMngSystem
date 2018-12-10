73d091c19ec6b4c0a1f6e205d9013917eb6b0749
Michael Walsh <calex71@gmail.com>
2018-12-10 19:27:01 +0000

    added code to add modify and delete users in the Admin page

--- LogInScreen/LogInScreen/admin.xaml.cs
+++ LogInScreen/LogInScreen/admin.xaml.cs
@@ -26,7 +26,16 @@ namespace LogInScreen
 
         List<User> users = new List<User>();
         List<Log> logs = new List<Log>();
+        User selectedUser = new User();
 
+        enum DBOperation
+        {
+            Add,
+            Modify,
+            Delete
+        }
+
+        DBOperation dBOperation = new DBOperation();
 
         public admin()
         {
@@ -37,19 +46,129 @@ namespace LogInScreen
         private void SubmnuAddNewUser_Click(object sender, RoutedEventArgs e)
         {
             stkUserDetails.Visibility = Visibility.Visible;
+            dBOperation = DBOperation.Add;
         }
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
-        {           
-            lstUserList.ItemsSource = users;
-            lstLogList.ItemsSource = logs;
-            foreach (var user in db.Users)
-            {
-                users.Add(user);                
-            }
+        {
+            RefreshUserList();
+            lstLogList.ItemsSource = logs;            
             foreach (var log in db.Logs)
             {
                 logs.Add(log);
+            }
+        }
+
+        private void BtnOK_Click(object sender, RoutedEventArgs e)
+        {
+            if (dBOperation == DBOperation.Add)
+            {
+                User user = new User();
+                user.Username = tbxUsername.Text.Trim();
+                user.Password = tbxPassword.Text.Trim();
+                user.Forename = tbxForename.Text.Trim();
+                user.Surname = tbxSurname.Text.Trim();
+                user.LevelID = cboAccessLevel.SelectedIndex;
+                int saveSuccess = SaveUser(user);
+                if (saveSuccess == 1)
+                {
+                    MessageBox.Show("User record saved successfully", "Save User Record", MessageBoxButton.OK, MessageBoxImage.Information);
+                    RefreshUserList();
+                    ClearUserDetails();
+                    stkUserDetails.Visibility = Visibility.Collapsed;
+                }
+                else
+                {
+                    MessageBox.Show("User record has not been saved", "Save User Record", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            if (dBOperation == DBOperation.Modify)
+            {
+                foreach (var user in db.Users.Where(t => t.UserID == selectedUser.UserID))
+                {
+                    user.Username = tbxUsername.Text.Trim();
+                    user.Password = tbxPassword.Text.Trim();
+                    user.Forename = tbxForename.Text.Trim();
+                    user.Surname = tbxSurname.Text.Trim();
+                    user.LevelID = cboAccessLevel.SelectedIndex;
+                }
+                int saveSuccess = db.SaveChanges();
+                if (saveSuccess == 1)
+                {
+                    MessageBox.Show("User record has been updated", "Save User Record", MessageBoxButton.OK, MessageBoxImage.Information);
+                    RefreshUserList();
+                    ClearUserDetails();
+                    stkUserDetails.Visibility = Visibility.Collapsed;
+                }
+            }
+        }
+
+        public int SaveUser(User user)
+        {
+            db.Entry(user).State = System.Data.Entity.EntityState.Added;
+            int saveSuccess = db.SaveChanges();
+            return saveSuccess;
+        }
+
+        // Refreshes the user list when application loads and is also called when a new record is added by pressing the OK button.
+        private void RefreshUserList()
+        {
+            lstUserList.ItemsSource = users;
+            users.Clear();
+            foreach (var user in db.Users)
+            {
+                users.Add(user);
+            }
+            lstUserList.Items.Refresh();
+        }
+
+        private void ClearUserDetails()
+        {
+            tbxUsername.Text = "";
+            tbxPassword.Text = "";
+            tbxForename.Text = "";
+            tbxSurname.Text = "";
+            cboAccessLevel.SelectedIndex = 0;
+        }
+
+        private void LstUserList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (lstUserList.SelectedIndex > 0)
+            {
+                selectedUser = users.ElementAt(lstUserList.SelectedIndex);
+               
+                    submnuModifySelectedUser.IsEnabled = true;
+                    submnuDeleteSelectedUser.IsEnabled = true;
+                    tbxUsername.Text = selectedUser.Username;
+                    tbxPassword.Text = selectedUser.Password;
+                    tbxForename.Text = selectedUser.Forename;
+                    tbxSurname.Text = selectedUser.Surname;
+                    cboAccessLevel.SelectedIndex = selectedUser.LevelID;
+                
+            }
+            
+        }
+
+        private void SubmnuModifySelectedUser_Click(object sender, RoutedEventArgs e)
+        {         
+            stkUserDetails.Visibility = Visibility.Visible;
+            dBOperation = DBOperation.Modify;
+        }
+
+        private void SubmnuDeleteSelectedUser_Click(object sender, RoutedEventArgs e)
+        {
+            db.Users.RemoveRange(db.Users.Where(t => t.UserID == selectedUser.UserID));
+            int saveSuccess = db.SaveChanges();
+            if (saveSuccess == 1)
+            {
+                MessageBox.Show("User record has been deleted", "Delete User Record", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                RefreshUserList();
+                ClearUserDetails();
+                stkUserDetails.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                MessageBox.Show("Problem deleting user record", "Delete User Record", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
     }
